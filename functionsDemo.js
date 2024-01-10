58ff1c5d2ee0f950cd0b1af895d7a23943fe4c34
bpoongs <49938919+bpoongs@users.noreply.github.com>
2024-01-10 15:12:27 +0530

    Add files via upload

--- functionsDemo.js
+++ functionsDemo.js
@@ -16,4 +16,41 @@ let sum = function(){
     document.getElementById("display").innerHTML = "The sum of " + number1 + " and " + number2 + " is " + output;
 }
 
-// 3rd way creating a function
+// 3rd way creating a function - arrow functions
+// let displayAlertArrow1 = function(){
+//     alert("message displayed by an arrow function!!");
+// }
+let displayAlertArrow = () => alert("message displayed by an arrow function!!");
+
+let arrayDestructure = () => {
+    let myDetails = ["123456", "abc@gmail.com", "address1"];
+
+    let myAddress = myDetails[2];
+    let myPhone = myDetails[0];
+    let myEmail = myDetails[1];
+
+    // can do it in an easier way using array destructuring
+    let [myPh, myEm, myAdd ] = myDetails;
+
+    console.log(myAddress, myPhone, myEmail);
+    console.log(myPh, myEm, myAdd);
+}
+
+let objectDestructure = () => {
+    // in JS we can create an object without a class(never possible in java)
+    let myProfile =  {
+        firstName: "John",
+        lastName: "Smith",
+        contact: 89898989
+    }  
+
+    let fName = myProfile.firstName;
+    let lName = myProfile.lastName;
+    let con = myProfile.contact;
+
+    // easier way is using obejct destructuring
+    let {firstName:fN, lastName:lN, contact:cont} = myProfile;
+    let {firstName, lastName, contact} = myProfile;
+    console.log(fName, lName, con);
+    console.log(fN, lN, cont);
+}
